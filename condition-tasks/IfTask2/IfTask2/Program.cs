feb125a79e37cc02e1b14f8cd35e68603a27bab5
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-09-20 09:18:47 +0300

    int.TryParse added

--- condition-tasks/IfTask2/IfTask2/Program.cs
+++ condition-tasks/IfTask2/IfTask2/Program.cs
@@ -9,14 +9,18 @@ namespace IfTask2
             Console.WriteLine("Ohjelma selvittää onko luku pariton vai parillinen.");
             Console.Write("Syötä numero: ");
             string userInput = Console.ReadLine();
-            int number = int.Parse(userInput);
+            int number = 0;
+            bool isNumber = int.TryParse(userInput, out number);
 
-            if (number % 2 == 0)
-                Console.WriteLine($"Luku {number} on parillinen.");
+            if (isNumber)
+            {
+                if (number % 2 == 0)
+                    Console.WriteLine($"Luku {number} on parillinen.");
+                else
+                    Console.WriteLine($"Luku {number} on pariton.");
+            }
             else
-                Console.WriteLine($"Luku {number} on pariton.");
-            //Console.WriteLine("Luku {0} on parillinen", number);
-            //Console.WriteLine("Luku " + number + " on parillinen");
+                Console.WriteLine("Väärä syöte! ");
 
 
         }
