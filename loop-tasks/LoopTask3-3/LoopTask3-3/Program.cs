3e2641a0c3dff042e4cedcd552adc353eba34293
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-10-04 21:46:16 +0300

    Loop Task 3.4 Initialized Loop Task 3.3 ready

--- loop-tasks/LoopTask3-3/LoopTask3-3/Program.cs
+++ loop-tasks/LoopTask3-3/LoopTask3-3/Program.cs
@@ -6,7 +6,31 @@ namespace LoopTask3_3
     {
         static void Main(string[] args)
         {
+            int number = 0;
+            int i = 1;
+            int evenSum = 0;
+            int oddSum = 0;
+            
             Console.WriteLine("Ohjelma laskee parittominen ja parillisten lukujen summan.");
+            Console.Write("Syötä luku: ");
+            number = int.Parse(Console.ReadLine());
+
+            while (i <= number)
+            {
+                if (i % 2 == 0)
+                {
+                    evenSum = evenSum + i; //evenSum += i;
+                }
+                else
+                {
+                    oddSum = oddSum + i;
+                }
+                i++;
+            }
+            Console.WriteLine($"Parillisten summa on {evenSum}");
+            Console.WriteLine($"Parittomien summa on {oddSum}");
+
+
         }
     }
 }
---
+++ loop-tasks/LoopTask3-4/LoopTask3-4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoopTask3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee positiivisten tai negatiivisten lukujen summan.");          
+        }
+    }
+}
