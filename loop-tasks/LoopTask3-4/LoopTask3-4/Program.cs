744d2c3e04c54a7851ef0d5c7860a4ca50403218
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-10-04 22:02:05 +0300

    Loop Task 3.4 ready

--- loop-tasks/LoopTask3-4/LoopTask3-4/Program.cs
+++ loop-tasks/LoopTask3-4/LoopTask3-4/Program.cs
@@ -6,7 +6,40 @@ namespace LoopTask3_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ohjelma laskee positiivisten tai negatiivisten lukujen summan.");          
+            //This algorithm based on flowchar by Rami Shemeikka look at in Moodle
+            int number = 0;
+            int i = 0;
+            int sum = 0;
+
+            Console.WriteLine("Ohjelma laskee N ensimmäistä lukua yhteen, myös negatiiviseen suuntaan.");
+            
+            do
+            {
+                Console.Write("Syötä luku: ");
+                bool isNumber = int.TryParse(Console.ReadLine(), out number); 
+                if (!isNumber)
+                {
+                    Console.Write("Virheellinen syöte!");
+                }
+                else
+                {
+                    break;
+                }
+            } while (true);            
+
+            while (i != number)
+            {
+                if (number > 0)
+                {
+                    i++;
+                }
+                else
+                {
+                    i--;
+                }
+                sum = sum + i;
+            }
+            Console.WriteLine($"Vastaus: {sum}");
         }
     }
 }
