5eedbca0113b14b6489676b40d35c4568f76ef33
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-10-04 08:29:36 +0300

    Loop Task 3.1 initialized

---
+++ loop-tasks/LoopTask-3-1/LoopTask-3-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoopTask_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
