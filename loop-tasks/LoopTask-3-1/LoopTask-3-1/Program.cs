64a4b208192dd9c224cccd6c5f709f1fe6108c4c
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-10-04 10:12:57 +0300

    Task 3.1 factorial algorithm addad - while loop

--- loop-tasks/LoopTask-3-1/LoopTask-3-1/Program.cs
+++ loop-tasks/LoopTask-3-1/LoopTask-3-1/Program.cs
@@ -6,7 +6,41 @@ namespace LoopTask_3_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma laskee annetun luvun kertoman.");
+            int number = 0;
+            int i = 1;
+            int fact = 1;
+
+            //Check that number is positive number
+            do
+            {
+                Console.Write("Syötä luku: ");
+                //string userInput = Console.ReadLine();
+                //number = int.Parse(userInput);
+                number = int.Parse(Console.ReadLine());
+
+                if (number <= 0)
+                {
+                    Console.WriteLine("Virheellinen syöte!");                   
+                }
+                else
+                {
+                    break;
+                }
+
+            } while (true);
+
+            // Calculate factorial 1 * 2 * 3 * ... * n
+            while (i <= number)
+            {
+                fact = fact * i;    //fact*=i
+                i = i + 1;          //i++;
+            }
+            //for (i = 1; i <= number; i++)
+            //{
+            //    fact *= i;
+            //}
+            Console.WriteLine($"Luvun {number}! = {fact}");
         }
     }
 }
