321b3c9ae6fc3172238626f657605a27ac522191
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-10-04 10:15:52 +0300

    Loop Task 3.3 initialized

---
+++ loop-tasks/LoopTask3-3/LoopTask3-3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoopTask3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee parittominen ja parillisten lukujen summan.");
+        }
+    }
+}
