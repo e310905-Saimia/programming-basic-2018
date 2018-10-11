c548650edd44dacdc90fbe9734c3bb94a41260cc
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-10-11 12:50:44 +0300

    LoopTask4.2 Initialized

---
+++ loop-tasks/LoopTask4-2/LoopTask4-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoopTask4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa kertotaulun luvuille 1-9");
+        }
+    }
+}
