f3d6d2bd92534d716d34f1ae52256f1bd87014db
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-10-11 12:51:44 +0300

    LoopTask3.5 done

--- loop-tasks/LoopTask3-5/LoopTask3-5/Program.cs
+++ loop-tasks/LoopTask3-5/LoopTask3-5/Program.cs
@@ -6,7 +6,47 @@ namespace LoopTask3_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = 0;
+            int i = 0;
+            int evenSum = 0;
+            int oddSum = 0;
+            string evenStr = string.Empty; //2+4+6+8+10 = 
+            string oddStr = string.Empty; //1+3+5+7+9 =
+            Console.WriteLine("Ohjelma laskee parittomien ja parillisten lukujen summan - huomio myös negatiiviset luvut");
+            Console.Write("Syötä luku: ");
+            number = int.Parse(Console.ReadLine());
+
+            while (i != number)
+            {
+                if (number > 0)
+                {
+                    i++;
+                }
+                else
+                {
+                    i--;
+                }
+
+                if (i % 2 == 0)
+                {
+                    evenSum += i;
+                    if (i != number)
+                        evenStr = $"{evenStr}{i} + ";
+                    else
+                        evenStr = $"{evenStr}{i}";
+
+                }
+                else
+                {
+                    oddSum += i;
+                    if (i != number - 1)
+                        oddStr = $"{oddStr}{i} + ";
+                    else
+                        oddStr = $"{oddStr}{i}";
+                }
+            }
+            Console.WriteLine($"{oddStr} = {oddSum}");
+            Console.WriteLine($"{evenStr} = {evenSum}");
         }
     }
 }
