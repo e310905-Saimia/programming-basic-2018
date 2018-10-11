1ac4c7c13a276f5e57eaf09a948f535de0637a80
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-10-11 13:10:53 +0300

    LoopTask 4.2 version 1 done

--- loop-tasks/LoopTask4-2/LoopTask4-2/Program.cs
+++ loop-tasks/LoopTask4-2/LoopTask4-2/Program.cs
@@ -7,6 +7,16 @@ namespace LoopTask4_2
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma tulostaa kertotaulun luvuille 1-9");
+            Console.WriteLine("Aloita ohjelman suoritus painamalla jotain näppäintä");
+            Console.ReadKey();
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    Console.WriteLine($"{i} x {j} = {i * j}");
+                }
+            }
+
         }
     }
 }
