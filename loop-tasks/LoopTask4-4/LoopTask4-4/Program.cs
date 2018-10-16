7ede18c3bb1e92c87811b0ab07f0f4f9bcc64c18
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-10-16 14:44:41 +0300

    LoopTask4.4 Initialized and done

---
+++ loop-tasks/LoopTask4-4/LoopTask4-4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LoopTask4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma simuloi kolikon heittoa!");
+            Console.Write("Kuinka monta kertaa kolikkoa heitetään? ");
+            int counter = int.Parse(Console.ReadLine());
+            int heads = 0;
+            int tails = 0;
+            Random rnd = new Random();
+            int rndNumber;
+            for (int i = 0; i < counter; i++)
+            {
+                rndNumber = rnd.Next(2);
+                if (rndNumber == 0)
+                    tails++;
+                else
+                    heads++;
+            }
+
+            Console.WriteLine($"Rahaa on heitetty {rndNumber} kertaa.");
+            Console.WriteLine($"Klaavoja tuli {tails} ja kruunuja {heads}.");
+        }
+    }
+}
