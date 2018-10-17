fc4685be3174e0713e6cd07212d49a0d39806f97
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-10-17 14:15:00 +0300

    FunctionExample done

---
+++ funtion-tasks/FunctionExample/FunctionExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FunctionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100, b = 80;
+            int c = Calculate(a, b);
+            PrintData(c);
+            IncrementValue(ref c);
+            PrintData(c);
+        }
+
+        static void PrintData(int value)
+        {
+            Console.WriteLine($"Vastaus: {value:F3}");
+        }
+
+        static int Calculate(int x, int y)
+        {
+            return x - y;
+        }
+
+        static void IncrementValue(ref int z)
+        {
+            z++;
+        }
+    }
+}
