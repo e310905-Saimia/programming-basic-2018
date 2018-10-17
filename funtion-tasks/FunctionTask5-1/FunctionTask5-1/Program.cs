8b2cd156607a61fcd3d3b2e1b553067351fca2b8
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-10-17 14:14:16 +0300

    FunctionTask5-1 Initialized and ready

---
+++ funtion-tasks/FunctionTask5-1/FunctionTask5-1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FunctionTask5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Functions - Task 1.\n");
+
+            int n;
+            while (true)
+            {
+                Console.Write("Tähtien lukumäärä: ");
+                n = int.Parse(Console.ReadLine());
+                if (n < 0)
+                {
+                    Console.WriteLine("Luku {0} ei ole sallittu luku", n);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            string asterisks = getNAsterisks(n);
+            Console.WriteLine(asterisks);
+            Console.ReadKey();
+        }
+
+        static string getNAsterisks(int n)
+        {
+            string res = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                res += '*';
+            }
+            return res;
+        }
+    }
+}
