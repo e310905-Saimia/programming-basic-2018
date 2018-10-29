9ad860cc1fffbd4ca971af8982ebe6259de05d3e
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-10-29 11:15:15 +0200

    FunctionTask3 Done

--- funtion-tasks/FunctionTask3/FunctionTask3/Program.cs
+++ funtion-tasks/FunctionTask3/FunctionTask3/Program.cs
@@ -6,7 +6,22 @@ namespace FunctionTask3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int retNumber = NumberFromRange(-10, 10);
+            Console.WriteLine($"Syötit sallitun luvun {retNumber}");
+        }
+
+        static int NumberFromRange(int lowerBound, int upperBound)
+        {
+            do
+            {
+                Console.Write($"Syötä luku alueelta {lowerBound} ... {upperBound} ");
+                string userInput = Console.ReadLine();                
+                bool isNumber = int.TryParse(userInput, out int userNumber);            
+                if (isNumber && userNumber >= lowerBound && userNumber <= upperBound)
+                    return userNumber;
+                else
+                    Console.WriteLine("Väärä syöte!");
+            } while (true);
         }
     }
 }
