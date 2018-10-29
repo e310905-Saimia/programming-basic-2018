f465571994466094ed756305360798699b17aa04
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-10-29 16:23:53 +0200

    FunctionTask4 Ref done

--- funtion-tasks/FunctionTask4/FunctionTask4/Program.cs
+++ funtion-tasks/FunctionTask4/FunctionTask4/Program.cs
@@ -6,7 +6,34 @@ namespace FunctionTask4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int indx=0;
+            Console.WriteLine("Syötä 10 lukua");
+            Console.WriteLine($"Suurin luku {LargesNumber(ref indx)} oli {indx}.");
+            //Console.WriteLine($"Suurin luku {LargesNumber(out int indx)} oli {indx}.");
+        }
+
+        static int LargesNumber(ref int indx)
+        {
+            //indx = 0;
+            int largesNumber=0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{i + 1}. ");
+                int userNumber = int.Parse(Console.ReadLine());
+                if (userNumber < 0)
+                {
+                    Console.WriteLine("Väärä syöte, syötä positiivinen luku");
+                    i--;
+                }
+
+                if (userNumber > largesNumber)
+                {
+                    largesNumber = userNumber;
+                    indx = i + 1;
+                }
+
+            }
+            return largesNumber;
         }
     }
 }
