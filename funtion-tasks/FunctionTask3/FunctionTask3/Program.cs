cc5c19d8ac4ab6d8b532c25a912a56fbb8d3e5d3
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-10-29 10:29:02 +0200

    FunctionTask3 Initialized

---
+++ funtion-tasks/FunctionTask3/FunctionTask3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunctionTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
