229fe1496c5ccfbc1a1fb1f77a0d69e205ac8211
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-10-31 13:52:20 +0200

    CultureInfo Example added

--- array-tasks/ArrayTask1/ArrayTask1/Program.cs
+++ array-tasks/ArrayTask1/ArrayTask1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ArrayTask1
 {
@@ -6,7 +7,32 @@ namespace ArrayTask1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CultureInfo culture = new CultureInfo("sv-SV");
+            CultureInfo.CurrentCulture = culture;
+            Console.WriteLine(DateTime.Now);
+
+            culture = new CultureInfo("fi-FI");
+            CultureInfo.CurrentCulture = culture;
+            Console.WriteLine(DateTime.Now);
+
+            culture = new CultureInfo("us-US");
+            CultureInfo.CurrentCulture = culture;
+            Console.WriteLine(DateTime.Now);
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Hinnasto");
+            
+            
+
+            decimal[] productPrice = new decimal[] { 7.96m, 72.0m, 99.90m };
+            int[] productAmount = new int[] {1,3,2};
+            decimal[] productTotalPrice = new decimal[3];
+
+            for (int i = 0; i < productTotalPrice.Length; i++)
+            {
+                productTotalPrice[i] = productAmount[i] * productPrice[i];
+                Console.WriteLine($"Tuote {i + 1}. {productTotalPrice[i]:c}");
+            }
         }
     }
 }
