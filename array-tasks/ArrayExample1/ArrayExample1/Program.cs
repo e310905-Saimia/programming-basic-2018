41ce6b99592b0c0712a5fdba2cc8fad91ad422d2
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-10-31 13:04:14 +0200

    ArrayExample1 Initialized and Ready

---
+++ array-tasks/ArrayExample1/ArrayExample1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ArrayExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma alustaa kokonaisluku taulukon (10 alkioita), johon arvotaan lukuja 0...100");
+            int[] numbers = new int[1000];
+            Random rnd = new Random();
+            
+            // Set value to array numbers
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(100);
+            }
+
+            // Get values from array numbers
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            Console.WriteLine("Ohjelman suoritus on päättynyt");
+        }
+    }
+}
