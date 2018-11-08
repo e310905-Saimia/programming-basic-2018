eb731e31ec154554b2d6cb67859ff69a08cbf581
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-11-08 11:31:52 +0200

    ArrayTask3-3 functions done. Need still some title function.

--- array-tasks/ArrayTask3-3/ArrayTask3-3/Program.cs
+++ array-tasks/ArrayTask3-3/ArrayTask3-3/Program.cs
@@ -6,7 +6,47 @@ namespace ArrayTask3_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arrT_1 = RndArray(10);
+            int[] arrT_2 = RndArray(10);
+            PrintData(arrT_1, "arrT_1:");
+            PrintData(arrT_2, "arrT_2:");
         }
+
+        /// <summary>
+        /// Fill array with random numbers
+        /// Set array lenght
+        /// Return filled array
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns>integer array</returns>
+        static int [] RndArray(int length)
+        {
+            Random rnd = new Random();
+            int[] arrT = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                arrT[i] = rnd.Next(20);
+            }
+            return arrT;
+        }
+
+        /// <summary>
+        /// Tulostaa taulukon tiedot näytölle.
+        /// arrX = taulukko
+        /// label = taulukon otsikko
+        /// </summary>
+        /// <param name="arrX"></param>
+        /// <param name="label"></param>
+        static void PrintData(int [] arrX, string label)
+        {
+            Console.Write($"{label} ");
+            for (int i = 0; i < arrX.Length; i++)
+            {
+                Console.Write($"{arrX[i]:d2} ");
+            }
+            Console.WriteLine();
+        }
+        
     }
 }
