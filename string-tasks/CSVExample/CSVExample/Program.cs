fece982b7eb8abb788f40b49b33a3c900c1da166
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-11-15 09:50:14 +0200

    CSV Example

---
+++ string-tasks/CSVExample/CSVExample/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace CSVExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma käsittelee pilkuilla erotetun datan.");
+            string data = "aasi,kahvi,väsymys,kaneli,aivot,tietokone,pettymys,torstai,pora,koodi,tietokone,18.";
+            //Versio1(data);
+            //Versio2(data);
+            string [] tmpData = Versio3(data);
+            PrintData(tmpData);
+
+        }
+
+        #region Versio 1
+        /// <summary>
+        /// Funktio käsittelee merkkijonon siten, että pilkut korvataan enter merkillä
+        /// d=merkkojono data, joka on pilkulla erotettua
+        /// </summary>
+        /// <param name="d"></param>
+        static void Versio1(string d)
+        {
+            string tmpData = d.Replace(',', '\n');
+
+            Console.WriteLine(d);
+            Console.WriteLine(tmpData);
+        }
+        #endregion
+
+        #region Versio 2
+        /// <summary>
+        /// Funktio etsii erotinmerkin ja tuolostaa erotinmerkkien välistä merkkijonon
+        /// </summary>
+        /// <param name="d"></param>
+        static void Versio2(string d)
+        {
+            int i = 0;
+            while (true)
+            {
+                int p = d.IndexOf(',', i);
+
+                if (p != -1)
+                {
+                    Console.WriteLine(d.Substring(i, p - i));
+                    i = p + 1;
+                }
+                else
+                {
+                    Console.WriteLine(d.Substring(i));
+                    break;
+                }
+            }
+        }
+        #endregion
+
+        #region Versio 3
+
+        /// <summary>
+        /// Funktio pilkkoo datan split metodia hyötyntäen
+        /// </summary>
+        /// <param name="d"></param>
+        static string[] Versio3(string d)
+        {
+            string[] tmpData = d.Split(',');
+            return tmpData;
+            //for (int i = 0; i < tmpData.Length; i++)
+            //{
+            //    Console.WriteLine(tmpData[i].ToUpper());
+            //}
+        }
+        #endregion
+
+        static void PrintData(string[] d)
+        {
+            for (int i = 0; i < d.Length; i++)
+            {
+                Console.WriteLine(d[i].ToUpper());
+            }
+
+        }
+    }
+}
