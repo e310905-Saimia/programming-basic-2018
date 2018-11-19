613e8a73ae0fc184ea283459331e6b303023d160
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-11-19 09:26:26 +0200

    StringExample Initialized and done

---
+++ string-tasks/StringExample/StringExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace StringExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello World!";
+            string tmpS = MyReplace(s);
+            tmpS = tmpS.Insert(s.Length - 1, "x");
+            int cntrL = CountLetter(s.ToUpper());
+
+            Console.WriteLine($"{s} {tmpS}");
+            Console.WriteLine($"Merkkijonossa {s} on L kirjaimia {cntrL} kappaletta");
+        }
+
+        static string MyReplace(string str)
+        {
+            return str.Replace("e", "@");
+        }
+
+
+
+
+
+
+
+
+
+        static int CountLetter(string str)
+        {
+            int letterCounter = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'L')
+                    letterCounter++;
+            }
+            return letterCounter;
+        }
+    }
+}
