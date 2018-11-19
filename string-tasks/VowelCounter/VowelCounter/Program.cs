eeea0625f256d3712e987441327b1de8db3107b9
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-11-19 09:05:49 +0200

    VowelCounter Initialized and version 1 done

---
+++ string-tasks/VowelCounter/VowelCounter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace VowelCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma selvittää sanassa tai lauseessa olevien vokaalien lukumäärän");
+            string userInput = UserInput();
+            string removedUserInput = VowelsRemove(userInput);
+            Console.WriteLine($"Vokaaleita sanassa {userInput} on {userInput.Length-removedUserInput.Length}");
+        }
+
+        static string UserInput()
+        {
+            Console.Write("Syötä sana tai lause: ");
+            return Console.ReadLine().ToUpper();
+        }
+
+        static string VowelsRemove(string d)
+        {
+            string vowels = "AEIOUYÄÖ";
+            for (int i = 0; i < d.Length; i++)
+            {
+                for (int j = 0; j < vowels.Length; j++)
+                {
+                    if (d[i] == vowels[j])
+                    {
+                        d = d.Remove(i, 1);
+                        i--;
+                        break;
+                    }
+                }
+                //if (d[i] == 'A' || d[i] == 'E' || d[i]=='I' || d[i] == 'O' || d[i] == 'U' || d[i] == 'Y' || d[i] == 'Ä' || d[i] == 'Ö')
+                //{                    
+                //    d = d.Remove(i, 1);
+                //    i--;
+                //}
+            }
+            return d;
+        }
+    }
+}
