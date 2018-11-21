f6f6d522f7c731b3e52c5228794b3cf96dab4e44
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-11-21 10:48:10 +0200

    FileClass Example done

---
+++ file-handling/ExampleFileHandling1/ExampleFileClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace ExampleFileClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read in every line in specified file.
+            // ... This will store all lines in an array in memory.
+            string[] lines = File.ReadAllLines(@"C:\TEMP\TESTI.DAT");
+            foreach (string line in lines)
+            {
+                // Do something with the line.
+                Console.WriteLine(line);
+                if (line.Length > 80)
+                {
+                    // Important code.
+                }
+            }
+        }
+    }
+}
