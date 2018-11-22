99bfe0ee509506c4109786e4d51685ae9974e1b2
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-11-22 08:48:23 +0200

    Solution 1 done Function Print stars

--- funtion-tasks/SelfEvaluateTask1/SelfEvaluateTask1/Program.cs
+++ funtion-tasks/SelfEvaluateTask1/SelfEvaluateTask1/Program.cs
@@ -6,7 +6,33 @@ namespace SelfEvaluateTask1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Tulostaa tähtiä.");
+            int[] numbers = new int[] { 2, 5, 10, 7, 3 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                PrintStars(numbers[i]);
+            }
+            //PrintStars(numbers[0]);
+            //PrintStars(numbers[1]);
+            //PrintStars(numbers[2]);
+            //PrintStars(numbers[3]);
+            //PrintStars(numbers[4]);
+        }
+
+
+        /// <summary>
+        /// Tulostaa tähtiä.
+        /// lenght = tähtien määrä
+        /// </summary>
+        /// <param name="length"></param>
+        static void PrintStars(int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
         }
     }
 }
