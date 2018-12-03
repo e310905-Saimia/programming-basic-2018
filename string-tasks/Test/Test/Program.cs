eb30a25ae3424b9e82f07de363dd906238b5087d
e3108905-saimia <jouni.k.kononen@gmail.com>
2018-12-03 09:52:19 +0200

    UITest Example done

---
+++ string-tasks/Test/Test/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string choise = null;
+            string path = @"C:\TEMP\TESTI.DAT";
+            string msg = "";
+            do
+            {
+                choise = UserInterface();
+
+                switch (choise.ToUpper())
+                {
+                    case "K":
+                        WriteToFile(path);
+                        msg = "\nTiedostoon kirjoitus on päättynyt! \nPaina Enter jatkaaksesi!";
+                        break;
+                    case "L":
+                        AddDataToFile(path);
+                        msg = "\nTiedoston loppuun kirjoitus on päättynyt! \nPaina Enter jatkaaksesi!";
+                        break;
+                    case "N":
+                        ReadFile(path);
+                        msg = "\nTiedoston lukeminen on päättynyt! \nPaina Enter jatkaaksesi!";
+                        break;
+                    case "X":
+                        msg = "\nOhjelman suoritus päättyy!";
+                        break;
+                    default:
+                        msg = "Nyt tuli huti yritä uudestaan - Paina Enter ja aloita alusta!";
+                        break;
+                }
+
+                Console.WriteLine(msg);
+                Console.ReadLine();
+                Console.Clear();
+            }
+            while (choise.ToUpper() != "X");
+
+        }
+
+
+      
+        static string UserInterface()
+        {
+            Console.WriteLine("Tiedoston käsittely esimerkki!");
+            Console.WriteLine("[K] Kirjoita tiedostoon - Tekee kokonaan uuden");
+            Console.WriteLine("[L] Kirjoita tiedoston loppuun - Lisätään tiedostoon tietoja");
+            Console.WriteLine("[N] Näytä tiedoston sisältö");
+            Console.WriteLine("[X] Lopeta ohjelmansuoritus");
+            Console.WriteLine();
+            Console.Write("Valitse mitä tehdään: ");
+
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Write to file
+        /// Create new file
+        /// string filePath is 
+        /// </summary>
+        /// <param name="filePath"></param>
+        static void WriteToFile(string filePath)
+        {
+            StreamWriter R = new StreamWriter(filePath);
+            R.WriteLine("TERVE TAAS!");
+            R.WriteLine(DateTime.Now.ToString());
+            R.WriteLine($"Tiedoston sijainti on: \t\t{filePath}");
+            R.Close();
+        }
+
+        static void ReadFile(string filePath)
+        {
+            StreamReader R = new StreamReader(filePath);
+            string strRow = null;
+
+            Console.WriteLine("Tiedostosta löytyy seuraavat rivit:\n");
+            while ((strRow = R.ReadLine()) != null)
+            {
+                Console.WriteLine(strRow);
+            }
+
+            R.Close();
+        }
+
+        /// <summary>
+        /// Append data to end of file
+        /// </summary>
+        /// <param name="filePath"></param>
+        static void AddDataToFile(string filePath)
+        {
+            StreamWriter R = new StreamWriter(filePath, true);
+            R.WriteLine("-----------------------------------------");
+            R.WriteLine("TERVE TAAS!");
+            R.WriteLine(DateTime.Now.ToString());
+            R.WriteLine("Tiedoston sijainti on: \t\t{0}", filePath);
+            R.Close();
+        }
+    }
+}
+
+
+
---
+++ string-tasks/Test/UITest/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace UITest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo cki;
+            string msg = string.Empty;
+            do
+            {            
+                cki = UserInterface();
+                switch (cki.Key)
+                {
+                    case ConsoleKey.F1:
+                        msg = "\nTiedostoon kirjoitus on päättynyt! \nPaina Enter jatkaaksesi!";
+                        break;
+
+                    case ConsoleKey.F2:
+                        msg = "\nTiedoston loppuun kirjoitus on päättynyt! \nPaina Enter jatkaaksesi!";
+                        break;
+                    case ConsoleKey.N:
+                        msg = "\nTiedoston lukeminen on päättynyt! \nPaina Enter jatkaaksesi!";
+                        break;
+                    case ConsoleKey.Escape:
+                        msg = "\nOhjelman suoritus päättyy!";
+                        break;
+                    default:
+                        msg = "Nyt tuli huti yritä uudestaan - Paina Enter ja aloita alusta!";
+                        break;
+                }                
+                Console.WriteLine(msg);
+                Console.ReadLine();
+                Console.Clear();
+            } while (cki.Key != ConsoleKey.Escape);
+
+            Console.WriteLine("Ohjelman suoritus päättyi!");
+        }
+
+
+        
+        static ConsoleKeyInfo UserInterface()
+        {            
+            Console.WriteLine("Tiedoston käsittely esimerkki!");
+            Console.WriteLine("[F1] Kirjoita tiedostoon - Tekee kokonaan uuden");
+            Console.WriteLine("[F2] Kirjoita tiedoston loppuun - Lisätään tiedostoon tietoja");
+            Console.WriteLine("[N] Näytä tiedoston sisältö");
+            Console.WriteLine("[ESC] Lopeta ohjelmansuoritus");
+            Console.WriteLine();
+            Console.Write("Valitse mitä tehdään: ");
+
+            return Console.ReadKey();
+        }
+    }
+}
